4d6aef599929d63b668dfe9668ec809f0c0216d0
Jenny <89851401+jennymeshaiah09@users.noreply.github.com>
2025-08-08 11:17:41 +0530

    Update ContactSection.tsx

--- frontend/components/Home/ContactSection.tsx
+++ frontend/components/Home/ContactSection.tsx
@@ -756,7 +756,7 @@ const ContactSection: React.FC = () => {
                     }}
                     onMouseOver={(e) => (e.target as HTMLElement).style.color = '#1a355e'}
                     onMouseOut={(e) => (e.target as HTMLElement).style.color = '#2ec4f1'}>
-                      +1 (234) 567-890
+                      +91 8697133050
                       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                       </svg>
